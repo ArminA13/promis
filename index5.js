62b9683f2c4cc391315d23b0afc38cec8a945177
Armine Muradyan <muradyan.armin13@gmail.com>
2023-11-04 23:01:13 +0400

    delete html file and add small change in index5 file

--- index5.js
+++ index5.js
@@ -44,16 +44,3 @@
 // .finally(res => console.log(res, 5))
 // .then((res) => console.log(res))
 // console.log(6)
-
-let user = {
-    name: "John",
-    age: 30,
-    isAdmin: true
-  };
-  
-  for (let key in user) {
-    // ключи
-    // console.log( key );  // name, age, isAdmin
-    // значения ключей
-    console.log( user[key] ); // John, 30, true
-  }
